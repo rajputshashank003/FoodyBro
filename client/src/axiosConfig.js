2f391d4386cfacf2bbd13546ec597473a7df054a
Shashank Rajput <hariomshashank@gmail.com>
2024-05-26 01:24:07 +0530

    update axios config

--- client/src/axiosConfig.js
+++ client/src/axiosConfig.js
@@ -1,3 +1,4 @@
-import axios from "axios";
-// axios.defaults.baseURL = "http://localhost:8080/";
-axios.defaults.baseURL = "http://192.168.18.69:8080";
+import axios from 'axios';
+
+axios.defaults.baseURL =
+  process.env.NODE_ENV !== 'production' ? 'http://192.168.18.69:8080' : '/';
