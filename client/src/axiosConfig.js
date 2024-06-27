b9aa51bfb0946ada82671474baa07df185cbf9e9
Shashank Rajput <hariomshashank@gmail.com>
2024-06-27 11:37:26 +0530

    fixed bugs

--- client/src/axiosConfig.js
+++ client/src/axiosConfig.js
@@ -1,4 +1,4 @@
 import axios from 'axios';
 
-axios.defaults.baseURL ="https://foodybroapi.vercel.app"
-  //process.env.NODE_ENV !== 'production' ? 'http://localhost:8080' : '/';
+axios.defaults.baseURL =
+  process.env.NODE_ENV !== 'production' ? 'http://localhost:8080' : '/';
