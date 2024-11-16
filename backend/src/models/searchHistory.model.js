082f52b60edf827df79e2a38cb2738bc2e39c9ae
Shashank Rajput <hariomshashank@gmail.com>
2024-11-16 13:24:46 +0530

    update vercel file

--- backend/src/models/searchHistory.model.js
+++ backend/src/models/searchHistory.model.js
@@ -4,6 +4,6 @@ export const searchHistorySchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref : "userModel" , required: true },
   searchTerm: { type: String, required: true },
   timestamp: { type: Date, default: Date.now },
-});
+}); 
 
 export const SearchHistory = mongoose.model('SearchHistory', searchHistorySchema);
