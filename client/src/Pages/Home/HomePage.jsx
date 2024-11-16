e051577fd75674f398e529a53510eab1a90f21ce
Shashank Rajput <hariomshashank@gmail.com>
2024-11-16 18:27:05 +0530

    final_testing...

--- client/src/Pages/Home/HomePage.jsx
+++ client/src/Pages/Home/HomePage.jsx
@@ -78,6 +78,7 @@ export default function HomePage() {
           sample_tags && sample_tags.length > 0 &&
           <div className={classes.tags}>
             <Tags tags={sample_tags}/>
+            <h1>testing...</h1>
           </div>
         }
         {
