fb76d2cd1565e08a51aa9c3bbe971c582befb8fd
1rajputshashank@gmail.com <hariomshashank@gmail.com>
2025-01-10 22:35:04 +0530

    fix home screen scroll stuck bug

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -13,7 +13,7 @@ import "./components/Interceptors/AuthInterceptor.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <div className='absolute min-h-svh w-screen bg-gradient-to-tr from-[#eda6a6] via-white'>
+    <div className=' min-h-svh pb-2 w-screen bg-gradient-to-tr from-[#eda6a6] via-white'>
     <BrowserRouter >
       <LoadingProvider>
         <AuthProvider>
