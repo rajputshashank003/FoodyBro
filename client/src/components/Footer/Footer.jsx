d73a1f64f20b8bb4e841866f32b1d6152dfaad1b
rajputshashank003 <hariomshashank@gmail.com>
2025-04-06 14:42:27 +0530

    add auto update year in footer

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
     return (
         <footer className="bg-[#D32F2F] rounded-md shadow m-4 mt-0">
             <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-            <span className="text-sm text-white sm:text-center ">© 2024 <a href="https://foodybro.vercel.app/" className="hover:underline">FoodyBro</a> All Rights Reserved.
+            <span className="text-sm text-white sm:text-center ">©{new Date().getFullYear()}<a href="https://foodybro.vercel.app/" className="hover:underline">FoodyBro</a> All Rights Reserved.
             </span>
             <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-white sm:mt-0">
                 <li>
